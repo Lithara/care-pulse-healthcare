f432d164e53406def842cda5bbe052a3d2c22238
Lithara Perera <litharaperera2002@gmail.com>
2024-07-31 12:02:08 +0530

    feat: Completed the project

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,40 +1,44 @@
-import AppointmentForm from "@/components/forms/AppointmentForm";
-import { getPatient } from "@/lib/actions/patient.actions";
 import Image from "next/image";
+import { getPatient } from "@/lib/actions/patient.actions";
+import AppointmentForm from "@/components/forms/AppointmentForm";
 
-export default async function NewAppointment({
-  params: { userId },
-}: SearchParamProps) {
+const Appointment = async ({ params: { userId } }: SearchParamProps) => {
   const patient = await getPatient(userId);
+
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[860px] flex-1 justify-between">
           <Image
             src="/assets/icons/logo-full.svg"
-            alt="patient"
-            width={1000}
             height={1000}
+            width={1000}
+            alt="logo"
             className="mb-12 h-10 w-fit"
           />
+
           <AppointmentForm
-            type="create"
+            patientId={patient?.$id}
             userId={userId}
-            patientId={patient.$id}
+            type="create"
+            setOpen={function (open: boolean): void {
+              throw new Error("Function not implemented.");
+            }}
           />
 
-          <p className="copyright mt-10 py-12">
-            © 2024 CarePulse
-          </p>
+          <p className="copyright mt-10 py-12">© 2024 CarePluse</p>
         </div>
       </section>
+
       <Image
         src="/assets/images/appointment-img.png"
+        height={1500}
+        width={1500}
         alt="appointment"
-        height={1000}
-        width={1000}
         className="side-img max-w-[390px] bg-bottom"
-      ></Image>
+      />
     </div>
   );
-}
+};
+
+export default Appointment;
